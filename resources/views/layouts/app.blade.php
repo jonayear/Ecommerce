a80b94395c2271a77a154931883fe892d5a58fc7
Jonayear <Jonayearimran@gmail.com>
2023-11-04 17:04:53 +0600

    add all required file in layouts file and add their path

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet" />
+    <script src="{{asset('asset/js/all.js')}}" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    <link href="{{asset('asset/css/bootstrap.min.css')}}" rel="stylesheet" />
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @livewireStyles
@@ -77,6 +79,13 @@
             @yield('content')
         </main>
     </div>
-     @livewireScripts
+    @livewireScripts
+    <script src="{{asset('asset/js/bootstrap.bundle.min.js')}}" crossorigin="anonymous"></script>
+    <script src="{{ asset('asset/js/scripts.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('asset/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ asset('asset/demo/chart-bar-demo.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/datatables-simple-demo.js') }}"></script>
 </body>
 </html>
